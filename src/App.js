c27ec27f1f4f4a35ef81522e03eb042802c48d5e
saraibosch <saraiboschcesar@gmail.com>
2022-02-03 01:33:23 -0300

    viendo otra vez segunda vez

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> otra vez por aca.
+          Edit <code>src/App.js</code> otra vez por aca con los cambios hechos aqui.
         </p>
         <a
           className="App-link"
