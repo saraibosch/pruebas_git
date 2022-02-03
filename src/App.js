f8b85dadee3779d18c5440ee074cef88d36f097c
saraibosch <saraiboschcesar@gmail.com>
2022-02-03 01:47:31 -0300

    luci1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          haciendo un buen cambio
           Edit <code>src/App.js</code> otra vez por aca con los cambios hechos ver.
         </p>
         <a
